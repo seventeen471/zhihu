c57d402504063a7f643213450f57a13355e21528
seventeen471 <3532394298@qq.com>
2019-09-07 16:24:04 +0800

    ting

---
+++ src/components/pageSearch.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="page-search">
+        <mt-search
+                :value.sync="value"
+                cancel-text="取消"
+                placeholder="搜索">
+        </mt-search>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "pageSearch"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/search.vue
@@ -0,0 +1,23 @@
+<template>
+    <div @click="open_page">点击我打开网页</div>
+</template>
+
+<script>
+    export default {
+        name: "search",
+        data: function () {
+            return {
+                url: 'https://www.zhihu.com/search?type=content&q=666'
+            }
+        },
+        methods: {
+            open_page: function () {
+                window.open(this.url)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
